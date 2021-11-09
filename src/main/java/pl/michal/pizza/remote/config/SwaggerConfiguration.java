114d95a19501939ac353fbf9e21b4693949bb549
MichałGutowski <michal.gutowski2002@gmail.com>
2021-11-09 15:52:36 +0100

    fixed swagger, added pizza controller

--- src/main/java/pl/michal/pizza/remote/config/SwaggerConfiguration.java
+++ src/main/java/pl/michal/pizza/remote/config/SwaggerConfiguration.java
@@ -10,7 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
-
+@Configuration
 public class SwaggerConfiguration {
     @Bean
     public Docket productApi() {
