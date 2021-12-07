9f71df959d79fc49fbcb2414cc13e400e0859796
MichałGutowski <michal.gutowski2002@gmail.com>
2021-12-07 14:16:20 +0100

    missing dto added

--- src/main/java/pl/michal/pizza/remote/rest/dto/response/OrderCollectionDto.java
+++ src/main/java/pl/michal/pizza/remote/rest/dto/response/OrderCollectionDto.java
@@ -1,4 +1,19 @@
 package pl.michal.pizza.remote.rest.dto.response;
+import java.util.List;
 
 public class OrderCollectionDto {
+
+    private List<OrderDto> orders;
+
+    public OrderCollectionDto(List<OrderDto> orders) {
+        this.orders = orders;
+    }
+
+    public List<OrderDto> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrderDto> orders) {
+        this.orders = orders;
+    }
 }
